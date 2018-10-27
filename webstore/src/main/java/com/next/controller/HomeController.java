745e0f44a91b3dd6a1876bf8ccda3c1e6f3d7706
ZayMaungMaungMyintDev <zaymaungmaungmyint.info@gmail.com>
2018-10-28 00:09:04 +0630

    Views and resolving views

--- webstore/src/main/java/com/next/controller/HomeController.java
+++ webstore/src/main/java/com/next/controller/HomeController.java
@@ -11,6 +11,11 @@ public class HomeController {
 	public String welcome(Model model) {
 		model.addAttribute("greeting", "Welcome to web store");
 		model.addAttribute("tagline", "The one and only amazing webstore");
+		return "forward:/welcome/greeting";
+	}
+
+	@RequestMapping("/welcome/greeting")
+	public String greeting() {
 		return "welcome";
 	}
 }
